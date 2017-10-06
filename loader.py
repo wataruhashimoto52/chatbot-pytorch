766f092196afea642efe8a658095742e7a4b1437
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-10-06 21:42:43 +0900

    add loader.py

---
+++ loader.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import torch
+import os
+import glob
+import numpy as np  
+
+def load_previous_model(encoder, decoder, checkpoint_dir, model_prefix):
+    pass
+
+def save_model(encoder, decoder, checkpoint_dir, model_prefix, epoch, max_keep = 5):
+    pass
