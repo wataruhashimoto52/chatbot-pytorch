efd49b550f369acf96ef760e080ce794dcddeddd
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-10-06 22:21:56 +0900

    update savemodel in loader.py

--- loader.py
+++ loader.py
@@ -9,4 +9,10 @@ def load_previous_model(encoder, decoder, checkpoint_dir, model_prefix):
     pass
 
 def save_model(encoder, decoder, checkpoint_dir, model_prefix, epoch, max_keep = 5):
-    pass
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    
+    f_list = glob.glob(os.path.join(checkpoint_dir, model_prefix) + '-*.pth')
+
+    if len(f_list) >=max_keep + 2:
+        pass
