99514864c5d917914d7acdb9e9e1b532fdc62ff1
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-10-07 11:34:24 +0900

    add prepareData to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -75,3 +75,12 @@ def variableFromSentence(vocab, sentence):
 
 def prepareData(lang1, lang2, reverse  = False):
     input_lang, output_lang, pairs = readText(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
