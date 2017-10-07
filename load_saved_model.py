21c28b43c66e0a262f4b9734503ef2869cf78802
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-10-07 19:36:57 +0900

    add load_saved_model.py

---
+++ load_saved_model.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+import torch
+from model import *
+from preprocess import *
+from global_config import *
+from train import *
+from loader import *
+
+
+if __name__ == "__main__":
+    
+    hidden_size = 256
+    input_lang, output_lang, pairs = prepareData('source', 'target', reverse = True)
+    encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
+    attn_decoder1 = AttentionDecoderRNN(hidden_size, output_lang.n_words,
+        n_layers = 1, dropout_p = 0.1)
+
+    l_encoder, l_decoder, epoch = load_previous_model(encoder1, attn_decoder1, 
+                                        CHECKPOINT_DIR, MODEL_PREFIX)
+
+    communication(input_lang, output_lang, l_encoder, l_decoder)
