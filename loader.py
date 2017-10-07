de38fd7bc1e41e7f8b75ba960e4a561ea5ade5e9
wh52 <cp.vlv.el.52wtr@gmail.com>
2017-10-07 13:41:13 +0900

    update savemodel in loader.py

--- loader.py
+++ loader.py
@@ -15,4 +15,15 @@ def save_model(encoder, decoder, checkpoint_dir, model_prefix, epoch, max_keep =
     f_list = glob.glob(os.path.join(checkpoint_dir, model_prefix) + '-*.pth')
 
     if len(f_list) >=max_keep + 2:
-        pass
+         # this step using for delete the more than 5 and litter one
+        epoch_list = [int(i.split('-')[-1].split('.')[0]) for i in f_list]
+        to_delete = [f_list[i] for i in np.argsort(epoch_list)[-max_keep:]]
+        for f in to_delete:
+            os.remove(f)
+    name = model_prefix + '-{}.pth'.format(epoch)
+    file_path = os.path.join(checkpoint_dir, name)
+    model_dict = {
+        'encoder': encoder.state_dict(),
+        'decoder': decoder.state_dict()
+    }
+    torch.save(model_dict, file_path)
